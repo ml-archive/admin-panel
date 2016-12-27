720e664c6477566854d8dc9b504eaa09e57d351a
Casper Rasmussen <cr@nodes.dk>
2016-12-27 13:29:10 +0100

    added doc blocks to each login controller action

--- Sources/AdminPanel/Controllers/LoginController.swift
+++ Sources/AdminPanel/Controllers/LoginController.swift
@@ -14,6 +14,12 @@ public final class LoginController {
         drop = droplet
     }
     
+    /**
+     * Landing page
+     *
+     * - param: Request
+     * - return: Response
+     */
     public func landing(request: Request) throws -> ResponseRepresentable {
         do {
             guard let user: BackendUser = try request.auth.user() as? BackendUser else {
@@ -26,10 +32,22 @@ public final class LoginController {
         }
     }
     
+    /**
+     * Reset password form
+     *
+     * - param: Request
+     * - return: View
+     */
     public func resetPasswordForm(request: Request) throws -> ResponseRepresentable {
         return try drop.view.make("Login/reset", for: request)
     }
     
+    /**
+     * Reset password submit
+     *
+     * - param: Request
+     * - return: View
+     */
     public func resetPasswordSubmit(request: Request) throws -> ResponseRepresentable {
         guard let email = request.data["email"]?.string else {
             throw Abort.custom(status: Status.badRequest, message: "Missing email")
@@ -48,12 +66,24 @@ public final class LoginController {
         return Response(redirect: "/admin/login").flash(.success, "Message sent")
     }
     
+    /**
+     * Login form
+     *
+     * - param: Request
+     * - return: View
+     */
     public func form(request: Request) throws -> ResponseRepresentable {
         return try drop.view.make("Login/login", [
             "next": request.query?["next"]?.node ?? nil
-            ], for: request)
+        ], for: request)
     }
     
+    /**
+     * Submit login
+     *
+     * - param: Request
+     * - return: Response
+     */
     public func submit(request: Request) throws -> ResponseRepresentable {
         // Get our credentials
         guard let username = request.data["email"]?.string, let password = request.data["password"]?.string else {
@@ -66,10 +96,8 @@ public final class LoginController {
             try request.auth.login(credentials)
             
             var redirect = "/admin/dashboard"
-            if let next: String = request.query?["next"]?.string{
-                if(next != "") {
-                    redirect = next
-                }
+            if let next: String = request.query?["next"]?.string, !next.isEmpty {
+                redirect = next
             }
             
             // Todo deal with remember me            
@@ -78,6 +106,5 @@ public final class LoginController {
         } catch {
             return Response(redirect: "/admin/login").flash(.error, "Failed to login")
         }
-        
     }
 }
