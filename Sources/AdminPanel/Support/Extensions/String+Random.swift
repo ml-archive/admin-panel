547eb07e0e9db94339fbbef688080871d76b1b0c
Casper Rasmussen <cr@nodes.dk>
2016-12-30 00:03:40 +0100

    fixed linux crash for random

--- Sources/AdminPanel/Support/Extensions/String+Random.swift
+++ Sources/AdminPanel/Support/Extensions/String+Random.swift
@@ -4,11 +4,16 @@ extension String {
     public static func randomAlphaNumericString(_ length: Int = 64) -> String {
         let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         let len = UInt32(letters.length)
-        
+        let max = len - 1
         var randomString = ""
         
         for _ in 0 ..< length {
-            let rand = arc4random_uniform(len)
+            #if os(Linux)
+                let rand = Int(random() % (max + 1))
+            #else
+                let rand = Int(arc4random_uniform(UInt32(max)))
+            #endif
+            
             var nextChar = letters.character(at: Int(rand))
             randomString += NSString(characters: &nextChar, length: 1) as String
         }
--- Tests/AdminPanelTests/AdminPanelTests.swift
+++ Tests/AdminPanelTests/AdminPanelTests.swift
@@ -1,15 +1,19 @@
 import XCTest
+import AdminPanel
 @testable import AdminPanelTests
 
 class AdminPanelTests: XCTestCase {
-    func test() {
-        XCTAssertTrue(true)
+    func testRandomString() {
+        let length = 10
+        let randomString = String.randomAlphaNumericString(length)
+        
+        XCTAssertEqual(randomString.characters.count, length)
     }
 
 
     static var allTests : [(String, (AdminPanelTests) -> () throws -> Void)] {
         return [
-            ("test", test),
+            ("testRandomString", testRandomString),
         ]
     }
 }
