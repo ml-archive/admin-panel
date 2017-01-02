dd972afc7bf160fe8a2deb41f80cc204b0661839
Casper Rasmussen <cr@nodes.dk>
2017-01-02 11:59:17 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
        // .Package(url: "https://github.com/nodes-vapor/storage", majorVersion: 0),
+         .Package(url: "https://github.com/bygri/vapor-forms.git", majorVersion: 0),
         .Package(url: "https://github.com/nodes-vapor/slugify", majorVersion: 1)
     ]
 )
