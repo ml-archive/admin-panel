6bc2b6c713cc47b2f26406a5c14ba8386c91174a
Casper Rasmussen <cr@nodes.dk>
2017-01-04 20:01:24 +0100

    updated package

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ let package = Package(
     name: "AdminPanel",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
-       // .Package(url: "https://github.com/nodes-vapor/storage", majorVersion: 0),
-         .Package(url: "https://github.com/bygri/vapor-forms.git", majorVersion: 0),
+        .Package(url: "https://github.com/nodes-vapor/sugar", majorVersion: 0),
+        .Package(url: "https://github.com/bygri/vapor-forms.git", majorVersion: 0),
         .Package(url: "https://github.com/nodes-vapor/slugify", majorVersion: 1)
     ]
 )
