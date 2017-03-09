7d862c4951b1b588e8cbe80a3b27038933d878c6
Casper Rasmussen <cr@nodes.dk>
2017-03-09 20:10:34 +0100

    added leaf tag for gate system

---
+++ Sources/AdminPanel/Support/LeafTags/Allow.swift
@@ -0,0 +1,31 @@
+import Leaf
+
+public final class Allow: Tag {
+    public let name = "allow"
+    
+    public enum Error: Swift.Error {
+        case expetedTwoArguments(have: [Argument])
+    }
+    
+    public func run(
+        stem: Stem,
+        context: Context,
+        tagTemplate: TagTemplate,
+        arguments: [Argument]) throws -> Node? {
+        guard arguments.count == 2 else { throw Error.expetedTwoArguments(have: arguments) }
+        return nil
+    }
+    
+    public func shouldRender(
+        stem: Stem,
+        context: Context,
+        tagTemplate: TagTemplate,
+        arguments: [Argument],
+        value: Node?) -> Bool {
+        guard let request = arguments.first?.value else { return false }
+        guard let backendUserRole = request["storage", "authedBackendUser", "role"]?.string else { return false }
+        guard let role = arguments[1].value?.string else { return false }
+        
+        return Gate.allow(backendUserRole, role)
+    }
+}
