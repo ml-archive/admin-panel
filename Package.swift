78c98df948b60498b44bfa1c59937ee2e550dfde
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2019-03-06 16:03:04 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -11,12 +11,12 @@ let package = Package(
             targets: ["AdminPanel"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/nodes-vapor/bootstrap.git", from: "3.0.0-beta"),
-        .package(url: "https://github.com/nodes-vapor/flash.git", from: "5.0.0-beta"),
-        .package(url: "https://github.com/nodes-vapor/paginator.git", from: "3.1.0"),
+        .package(url: "https://github.com/nodes-vapor/bootstrap.git", from: "4.0.0-rc"),
+        .package(url: "https://github.com/nodes-vapor/flash.git", from: "5.0.0-rc"),
+        .package(url: "https://github.com/nodes-vapor/paginator.git", from: "3.2.0"),
         .package(url: "https://github.com/nodes-vapor/reset.git", from: "1.0.0-rc"),
-        .package(url: "https://github.com/nodes-vapor/submissions.git", from: "2.0.0-beta"),
-        .package(url: "https://github.com/nodes-vapor/sugar.git", from: "4.0.0-beta"),
+        .package(url: "https://github.com/nodes-vapor/submissions.git", from: "2.0.0-rc"),
+        .package(url: "https://github.com/nodes-vapor/sugar.git", from: "4.0.0-rc"),
 
         .package(url: "https://github.com/twof/VaporMailgunService.git", from: "1.1.0"),
 
